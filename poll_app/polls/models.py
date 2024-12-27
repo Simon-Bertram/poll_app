48aec26f49f79c52616e86089754f01f4e217543
Simon-Bertram <simonbertram@hotmail.com>
2024-12-27 10:15:49 +0000

    Add PollsConfig to the installed apps

--- poll_app/polls/models.py
+++ poll_app/polls/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class Question(models.model):
+class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
-class Choice(models.model):
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
