dd4cbd3ae018d86999e1c89c6eecd95e55aebdcd
Simon-Bertram <simonbertram@hotmail.com>
2024-12-27 11:07:25 +0000

    Add custom method on the Question model to calculate if was_published_recently is true or false

--- poll_app/polls/models.py
+++ poll_app/polls/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return self.pub_date >= (now - datetime.timedelta(days=1)).timestamp()
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
