46ca26f95d5744c62229d2f52b5312db23a997a0
Simon-Bertram <simonbertram@hotmail.com>
2025-01-05 15:15:28 +0000

    Change the choice fields to display TabularInline instead of StackedInline

--- poll_app/polls/admin.py
+++ poll_app/polls/admin.py
@@ -3,7 +3,17 @@ from .models import Question, Choice
 
 # Register your models here.
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
+
+
